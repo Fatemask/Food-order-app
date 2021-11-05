7236106b8dcdc39e7f878e1d77f418eb256fbc7f
Fatemask <fatemask29@gmail.com>
2021-11-05 20:58:51 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import Header from "./Components/Layout/Header/Header";
-import './App.css';
-import Meals from './Components/Meals/Meals';
+import { useState } from 'react';
 
-const App= props => {
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-   <React.Fragment>
-     <Header/>
-     <main>
-        <Meals/>
-     </main>
-   </React.Fragment>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
